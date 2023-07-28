764d9f20e893d4a05c4a547fd0119bfd5455dcfb
rejkid <rejkid@gmail.com>
2023-07-29 00:52:52 +1000

    Msg

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 
-var app = express(); // nothing 
+var app = express(); // nothing else
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
